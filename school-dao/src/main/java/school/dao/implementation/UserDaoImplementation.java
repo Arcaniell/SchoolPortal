19aecd41f10bfcf94713f61392592122b65e34bf
romamr <rmudrij@mail.ru>
2014-10-14 19:05:37 +0300

    Added stub files for the empty forders + returned mistakenly deleted fiel school-web/.settings/org.eclipse.wst.common.component

---
+++ school-dao/src/main/java/school/dao/implementation/UserDaoImplementation.java
@@ -0,0 +1,33 @@
+package school.dao.implementation;
+
+import school.dao.UserDao;
+import school.model.User;
+
+public class UserDaoImplementation implements UserDao{
+
+	public User findById(User entity) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void save(User entity) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void remove(User entity) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public User update(User entity) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public User findByName(User user) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ school-dao/src/test/java/school/dao/implementation/UserDaoImplementationTest.java
@@ -0,0 +1,14 @@
+package school.dao.implementation;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class UserDaoImplementationTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
