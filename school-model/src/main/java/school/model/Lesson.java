12dd4dab2d475dc54436b54ec35316c32f00fcda
KalinkOs <kalinkoc@gmail.com>
2014-10-15 19:43:51 +0300

    lesson entry

--- school-model/src/main/java/school/model/Lesson.java
+++ school-model/src/main/java/school/model/Lesson.java
@@ -1,11 +1,15 @@
 package school.model;
 import java.util.Date;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -13,13 +17,16 @@ import javax.persistence.Table;
 public class Lesson {
 
 @Id
-@Column(name="id")
+@Column(name="id", unique = true)
 @GeneratedValue(strategy=GenerationType.AUTO)
 	private int id;
 
 @Column(name="startTime")
-// в базі Datetime
 private Date startTime;
+
+@OneToMany(fetch = FetchType.LAZY)
+@JoinColumn(name="id")
+private List<Schedule> listSchedule;
 
 public void setLesId(int id){
 	this.id = id;
@@ -29,6 +36,10 @@ public void setLesStartTime(Date startTime){
 	this.startTime = startTime;
 }
 
+public void setListSchedule(List<Schedule> listSchedule){
+	this.listSchedule = listSchedule;
+}
+
 public int getLesId() {
     return id;
 }
@@ -36,5 +47,12 @@ public int getLesId() {
 public Date getLesStartTime(){
 	return startTime;
 }
+
+public List<Schedule> getListSchedule() {
+	return listSchedule;
+}
+
+
+
 
 }
