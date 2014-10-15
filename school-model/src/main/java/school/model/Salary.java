ab5d16116994bbc7521a847b359d4746b04e7a22
rzhereb <roman.zherebetskyi@gmail.com>
2014-10-15 19:37:44 +0300

    Salary Edit

--- school-model/src/main/java/school/model/Salary.java
+++ school-model/src/main/java/school/model/Salary.java
@@ -8,6 +8,7 @@ import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -15,41 +16,46 @@ import javax.persistence.TemporalType;
 @Entity
 public class Salary {
 	@Id
-	@GeneratedValue
 	private int id;
 	@Column
 	private double sum;
 	@Column
 	@Temporal(TemporalType.DATE)
 	private Date issueDate;
-	@OneToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name="teacherId")
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "teacherId")
 	private Teacher teacher;
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public double getSum() {
 		return sum;
 	}
+
 	public void setSum(double sum) {
 		this.sum = sum;
 	}
+
 	public Date getIssueDate() {
 		return issueDate;
 	}
+
 	public void setIssueDate(Date issueDate) {
 		this.issueDate = issueDate;
 	}
+
 	public Teacher getTeacher() {
 		return teacher;
 	}
+
 	public void setTeacher(Teacher teacher) {
 		this.teacher = teacher;
 	}
-	
-	
-	
+
 }
