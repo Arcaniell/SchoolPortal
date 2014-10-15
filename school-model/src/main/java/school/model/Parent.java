7f99d346f9cf94b9bf518afec448d429fcadb82c
LeatherOWL <odumaytess@gmail.com>
2014-10-15 16:13:40 +0300

    Message and Parent modify

--- school-model/src/main/java/school/model/Parent.java
+++ school-model/src/main/java/school/model/Parent.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToMany;
@@ -14,7 +15,7 @@ import javax.persistence.OneToOne;
 @Entity
 public class Parent {
 	@Id
-	@GeneratedValue
+	@GeneratedValue (strategy=GenerationType.IDENTITY)
 	private long id;
 	@OneToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "userId")
@@ -54,5 +55,4 @@ public class Parent {
 	public void setStudents(List<Student> students) {
 		this.students = students;
 	}
-	
 }
