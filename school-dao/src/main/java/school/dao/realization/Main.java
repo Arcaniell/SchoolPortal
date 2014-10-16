fca154131cec3933e5ad3497fe75948fc5da9013
blowder <fedinchuk.viktor@mail.ru>
2014-10-16 20:49:45 +0300

    Hibernate factory was closed

--- school-dao/src/main/java/school/dao/realization/Main.java
+++ school-dao/src/main/java/school/dao/realization/Main.java
@@ -9,5 +9,6 @@ public class Main {
 	public static void main(String[] args) {
 		Session session = HibernateSessionFactory.getSessionFactory().openSession();
 		session.close();
+		HibernateSessionFactory.shutdown();
 	}
 }
