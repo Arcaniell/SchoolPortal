21773c18f667c882f8839f3ca882db225a5ede2c
vsomov <zemnovodnoe@gmail.com>
2014-10-16 17:45:34 +0300

    Teacher & Subject.java added not nullable fields to tables

--- school-model/src/main/java/school/model/Subject.java
+++ school-model/src/main/java/school/model/Subject.java
@@ -1,15 +1,32 @@
 package school.model;
 
+import java.util.List;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 @Entity
 public class Subject {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	long id;
+	private long id;
+	private int rate;
+	@Column(nullable=false)
+	private String subject;
+	@Column(nullable=false)
+	private int groupNumber;
+	@Column(nullable=false)
+	private double coefficient;
+	private double price;
+	@Column(nullable=false)
+	private boolean additional;
+	
+	@ManyToMany(mappedBy="subjects")
+	List<Teacher> teachers;
 
 	public long getId() {
 		return id;
--- school-model/src/main/java/school/model/Teacher.java
+++ school-model/src/main/java/school/model/Teacher.java
@@ -1,13 +1,18 @@
 package school.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
@@ -20,14 +25,19 @@ public class Teacher {
 	@Column(name="id")
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
-	@Column(name="rate")
 	private int rate;
 	@OneToMany(mappedBy = "teacher")
 	private List<Salary> salaries;
 	@OneToOne
-	@JoinColumn(name="userId")
+	@JoinColumn(nullable=false, name="userId")
 	private User user;
-
+	@ManyToMany
+	
+	@JoinTable(name = "TeacherSubjectRef", 
+	joinColumns = { @JoinColumn(name = "teacherId", nullable = false) }, 
+	inverseJoinColumns = { @JoinColumn(name = "subjectId", nullable = false)})
+	
+	List<Subject> subjects = new ArrayList<Subject>();
 	public Teacher() {
 		super();
 		// TODO Auto-generated constructor stub
