0ad23d878c1b0594a38b90bb4f0125e9360b878a
romamr <rmudrij@mail.ru>
2014-10-16 13:47:32 +0300

    removed password, fixed Teacher.java

--- school-model/src/main/java/school/model/Teacher.java
+++ school-model/src/main/java/school/model/Teacher.java
@@ -1,11 +1,14 @@
 package school.model;
 
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
@@ -19,30 +22,47 @@ public class Teacher {
 	private long id;
 	@Column(name="rate")
 	private int rate;
+	@OneToMany(mappedBy = "teacher")
+	private List<Salary> salaries;
 	@OneToOne
-	@JoinColumn(name="id")
-	private Salary salary;
-	@OneToOne
-	@JoinColumn(name="id")
-	private Group2 group;
-	@OneToOne
-	@JoinColumn(name="id")
+	@JoinColumn(name="userId")
 	private User user;
-	public long getId() {
-		return id;
-	}
-	public void setId(long id) {
-		this.id = id;
-	}
-	public int getRate() {
-		return rate;
-	}
-	public void setRate(int rate) {
-		this.rate = rate;
-	}
+
 	public Teacher() {
 		super();
 		// TODO Auto-generated constructor stub
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public int getRate() {
+		return rate;
+	}
+
+	public void setRate(int rate) {
+		this.rate = rate;
+	}
+
+	public List<Salary> getSalaries() {
+		return salaries;
+	}
+
+	public void setSalaries(List<Salary> salaries) {
+		this.salaries = salaries;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
 	}
 	
 }
