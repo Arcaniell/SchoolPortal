f7c64f8e547f92a563bdf0a0e8aea8ab0014439a
romamr <rmudrij@mail.ru>
2014-10-31 03:10:26 +0200

    web module configurations

--- school-dao/src/main/java/school/dao/implementation/UserDaoImpl.java
+++ school-dao/src/main/java/school/dao/implementation/UserDaoImpl.java
@@ -1,5 +1,7 @@
 package school.dao.implementation;
 
+import javax.persistence.NoResultException;
+
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,13 +16,21 @@ public class UserDaoImpl extends BaseDaoImpl<User, Long> implements UserDao {
 	}
 	@Transactional
 	public User findByEmail(String email) {
+		try{
 		return (User) entityManager.createNamedQuery(User.FIND_BY_EMAIL)
 				.setParameter("email", email).getSingleResult();
+		}catch(NoResultException e){
+			return null;
+		}
 	}
 	@Transactional
 	public User findByEmailAndPassword(String email, String password) {
+		try{
 		return (User) entityManager.createNamedQuery(User.FIND_BY_EMAIL_AND_PASSWORD)
 				.setParameter("email", email)
 				.setParameter("password", password).getSingleResult();
+		}catch(NoResultException e){
+			return null;
+		}
 	}
 }
--- school-service/src/main/java/school/service/implementation/UserServiceImpl.java
+++ school-service/src/main/java/school/service/implementation/UserServiceImpl.java
@@ -2,18 +2,24 @@ package school.service.implementation;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import school.dao.UserDao;
 import school.model.User;
 import school.service.UserService;
 
 @Service
-public class UserServiceImpl implements UserService{
+public class UserServiceImpl implements UserService {
+
 	@Autowired
 	private UserDao userDao;
+	
+	@Transactional
 	public User createUser(User user) {
-		userDao.save(user);
-		return userDao.findById(1L);
+		if (userDao.findByEmail(user.getEmail()) == null) {
+			return userDao.update(user);
+		} else
+			return null;
 	}
 
 }
