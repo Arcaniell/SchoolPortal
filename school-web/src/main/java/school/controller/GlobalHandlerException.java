bb4e8b301a94d5a61c4d2f62a336354cf42fb711
blowder <fedinchuk.viktor@mail.ru>
2014-12-18 23:34:28 +0200

    Database connection restoration added Changes to be committed: 	modified:   school-dao/pom.xml 	modified:   school-dao/src/main/resources/META-INF/persistence.xml 	modified:   school-web/src/main/java/school/controller/GlobalHandlerException.java

--- school-web/src/main/java/school/controller/GlobalHandlerException.java
+++ school-web/src/main/java/school/controller/GlobalHandlerException.java
@@ -1,7 +1,9 @@
 package school.controller;
 
+import java.net.ConnectException;
 import java.sql.SQLException;
 
+import javax.persistence.PersistenceException;
 import javax.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
@@ -9,30 +11,37 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.util.NestedServletException;
+
+import com.mysql.jdbc.exceptions.MySQLNonTransientConnectionException;
 
 @ControllerAdvice
 public class GlobalHandlerException {
 
-	private static final Logger logger = LoggerFactory
-			.getLogger(GlobalHandlerException.class);
+    private static final Logger logger = LoggerFactory.getLogger(GlobalHandlerException.class);
 
-	@ExceptionHandler(NullPointerException.class)
-	public ModelAndView handleNullPointerException(HttpServletRequest request,
-			Exception exception) {
-		logger.info("NullPointerException:: URL=" + request.getRequestURL());
-		ModelAndView model = new ModelAndView("errorPage");
-		model.addObject("message",
-				"Sorry, there is no data available for your request");
-		model.addObject("exc", exception.toString());
+    @ExceptionHandler(NullPointerException.class)
+    public ModelAndView handleNullPointerException(HttpServletRequest request, Exception exception) {
+        logger.info("NullPointerException:: URL=" + request.getRequestURL());
+        ModelAndView model = new ModelAndView("errorPage");
+        model.addObject("message", "Sorry, there is no data available for your request");
+        model.addObject("exc", exception.toString());
 
-		return model;
-	}
+        return model;
+    }
 
-	@ExceptionHandler(SQLException.class)
-	public ModelAndView handleSQLException(HttpServletRequest request,
-			Exception exception) {
-		ModelAndView model = new ModelAndView(request.getRequestURI());
-		model.addObject("message", "exception");
-		return model;
-	}
+    @ExceptionHandler(SQLException.class)
+    public ModelAndView handleSQLException(HttpServletRequest request, Exception exception) {
+        ModelAndView model = new ModelAndView(request.getRequestURI());
+        model.addObject("message", "exception");
+        return model;
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ModelAndView handleSQLException2(HttpServletRequest request, Exception exception) {
+        ModelAndView model = new ModelAndView("errorPage");
+        model.addObject("message", "Sorry, there is no data available for your request");
+        model.addObject("exc", exception.toString());
+        return model;
+    }
 }
